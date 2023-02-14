86cc18ec803bcd70875d4398baa4b8734d15717f
kevohm <kevinkbet@gmail.com>
2023-02-14 19:08:11 +0000

    fixed error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,17 +43,16 @@ def login():
     return abort(401)
 
 
-@app.route("/sessions", methods=['DELETE'])
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
-    """ logout user
-    """
-    sess_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(sess_id)
+    """Logout user"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect(url_for("hello"))
+        return redirect(url_for('welcome'))
     else:
-        return abort(403)
+        abort(403)
 
 
 if __name__ == "__main__":

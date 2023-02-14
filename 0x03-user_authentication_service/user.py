dd05894333bc27d647d56db75b652254e9e19cff
kevohm <kevinkbet@gmail.com>
2023-02-14 10:20:25 +0000

    fixed pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-""" user
+""" user module
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+
 
 Base = declarative_base()
 
 
 class User(Base):
+    """User class
+    """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)

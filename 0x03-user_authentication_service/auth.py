28c5ba47720c5e14df2275a3e98523a15c6e7436
kevohm <kevinkbet@gmail.com>
2023-02-14 12:33:47 +0000

    finished task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,3 +13,21 @@ def _hash_password(password: str) -> str:
     """Hash a password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register users
+        """
+        hashed = _hash_password(password)
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(user.email))
+        except(NoResultFound):
+            return self._db.add_user(email=email, hashed_password=hashed)

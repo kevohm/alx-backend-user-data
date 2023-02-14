9ea5d76ad40992eeecc396de5b9c57ea3f430163
kevohm <kevinkbet@gmail.com>
2023-02-14 10:02:45 +0000

    fixed nullable

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -7,4 +7,4 @@ from user import User
 print(User.__tablename__)
 
 for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+    print("{}: {}, {}".format(column, column.type, column.nullable))

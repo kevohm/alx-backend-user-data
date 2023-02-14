1888f362266a070263d6f3e0a7f81c5d9bc4070b
kevohm <kevinkbet@gmail.com>
2023-02-14 19:39:18 +0000

    finished task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,17 @@ def profile():
     return abort(403)
 
 
+@app.route("/reset_password", methods=['POST'], strict_slashes=False)
+def reset():
+    """get reset pasword token
+    """
+    email = request.form['email']
+    try:
+        uid = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": uid})
+    except ValueError:
+        return abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

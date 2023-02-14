0e83af6cee514bd0deaabcf6754aa1602435d7a9
kevohm <kevinkbet@gmail.com>
2023-02-14 18:04:15 +0000

    fixed pycodestyle compliant

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,10 +14,12 @@ def _hash_password(password: str) -> str:
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
+
 def _generate_uuid() -> str:
     """generate uuid
     """
     return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.

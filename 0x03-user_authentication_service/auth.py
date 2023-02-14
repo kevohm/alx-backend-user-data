07c92acf7ecc82146b55101d0d9dd890bfa9984c
kevohm <kevinkbet@gmail.com>
2023-02-14 17:28:28 +0000

    finished task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,5 +29,14 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             raise ValueError("User {} already exists".format(user.email))
-        except(NoResultFound):
+        except NoResultFound:
             return self._db.add_user(email=email, hashed_password=hashed)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if login is valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password, user.password)
+        except NoResultFound:
+            return False

6a1eceeebff041515a5588699924a668de22f543
kevohm <kevinkbet@gmail.com>
2023-02-14 18:32:33 +0000

    finished task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """app module
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -23,6 +23,18 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    if(AUTH.valid_login(email, password)):
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": email, "message": "logged in"})
+        res.set_cookie('session_id', session_id)
+        return res
+    return abort(401)
 
 
 if __name__ == "__main__":

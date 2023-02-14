d0b653f043989080f797fba37aaaaf4d3980de80
kevohm <kevinkbet@gmail.com>
2023-02-14 19:33:09 +0000

    finshed task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,5 +85,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
             uid = _generate_uuid()
             self._db.update_user(user.id, reset_token=uid)
+            return uid
         except NoResultFound:
             raise ValueError

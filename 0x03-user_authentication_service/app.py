c722bbeca1d20520815597c291603596d1bf01f6
kevohm <kevinkbet@gmail.com>
2023-02-14 19:09:45 +0000

    fixed error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,7 +50,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect(url_for('welcome'))
+        return redirect(url_for('hello'))
     else:
         abort(403)
 

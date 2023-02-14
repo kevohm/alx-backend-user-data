16d9bc0bb56d77ac15676fd9117142f3253c3c18
kevohm <kevinkbet@gmail.com>
2023-02-14 11:45:58 +0000

    fixed task 0

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add new user
+        """Add a user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
